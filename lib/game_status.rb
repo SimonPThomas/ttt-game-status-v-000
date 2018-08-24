e60915f281724ac119347126ff09d02c4e7b6cd5
Simon Thomas <simonpthomas89@gmail.com>
2018-08-24 18:21:13 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,7 +38,19 @@ end
 
 def draw? (board)
   if board.any? { |empty| empty != " " }
-   true
- else false
- end
+   return true
+  if  WIN_COMBINATIONS.each do |combo| # combo =[3, 4, 5]
+     position = combo[0] #postion = 3
+     position_2 = combo[1] #postion = 4
+     position_3 = combo[2] #postion = 5
+
+     cell = board[position] # "X" || "O"
+     cell_2 = board[position_2]
+     cell_3 = board[position_3]
+
+     cell == cell_2 == cell_3 && position_taken?(board, position)
+  return false
+  if board.any? { |empty| empty == " " }
+  return false
+  end
 end
