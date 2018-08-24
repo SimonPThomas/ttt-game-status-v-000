c1e1b22986c2bf35a35576178c44fe1b733de42a
Simon Thomas <simonpthomas89@gmail.com>
2018-08-24 19:53:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -63,21 +63,21 @@ def over?(board)
 end
 
 def winner(board)
-  # WIN_COMBINATIONS.each do |combo| # combo =[3, 4, 5]
-  #   position = combo[0] #postion = 3
-  #   position_2 = combo[1] #postion = 4
-  #   position_3 = combo[2] #postion = 5
-  #
-  #   cell = board[position] # "X" || "O"
-  #   cell_2 = board[position_2]
-  #   cell_3 = board[position_3]
-  #
-  #   if cell == cell_2 && cell_2 == cell_3 && position_taken?(board, position)
-if won?(board) == true
+  WIN_COMBINATIONS.each do |combo| # combo =[3, 4, 5]
+    position = combo[0] #postion = 3
+    position_2 = combo[1] #postion = 4
+    position_3 = combo[2] #postion = 5
+  
+    cell = board[position] # "X" || "O"
+    cell_2 = board[position_2]
+    cell_3 = board[position_3]
+  
+    if cell == cell_2 && cell_2 == cell_3 && position_taken?(board, position)
+# if won?(board) == true
 return "#{cell}"
 end
 if full?(board) == true
 return nil
-# end
+end
 end
 end
