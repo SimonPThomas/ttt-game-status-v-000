351b123b01370370631114c62b44df1880169fd7
Simon Thomas <simonpthomas89@gmail.com>
2018-08-24 18:21:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,6 +39,7 @@ end
 def draw? (board)
   if board.any? { |empty| empty != " " }
    return true
+ end
   if  WIN_COMBINATIONS.each do |combo| # combo =[3, 4, 5]
      position = combo[0] #postion = 3
      position_2 = combo[1] #postion = 4
@@ -50,6 +51,7 @@ def draw? (board)
 
      cell == cell_2 && cell_2 == cell_3 && position_taken?(board, position)
   return false
+end
   if board.any? { |empty| empty == " " }
   return false
   end
